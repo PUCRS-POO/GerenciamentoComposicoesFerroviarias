7faa4d6e00828f9e8025330e7f4721b7b2c67605
Luiz Eduardo Ferreira Da Costa Carvalho <19180149@pucrs.br>
2019-09-02 11:30:37 -0300

    engataLocomotiva e engataVagao

--- Composicao.java
+++ Composicao.java
@@ -41,11 +41,40 @@ public class Composicao {
 
 	public boolean engataLocomotiva(Locomotiva locomotiva) {
 		// TO DO: escrever o código que permite engatar uma locomotiva
-		return true;
+		if(getQtdadeVagoes == 0 && locomotiva.getComposicao() == -1) {
+			locomotivas.add(locomotiva);
+			locomotiva.setComposicao(this);
+			return true;
+		}
+		return false;
 	}
 
 	public boolean engataVagao(Vagao vagao) {
 		// TO DO: escrever o código que permite engatar um vagao
+		if(getQtdadeLocomotivas > 0) {
+			int numeroTotalVagoes = 0;
+			for(int i = 0; i < this.getQtdadeLocomotivas(); i++) {
+				numeroTotalVagoes += (this.getLocomotiva(i).getQtdadeMaxVagoes());
+			}
+			if(this.getQtdadeLocomotivas > 1) {
+				numeroTotalVagoes -= (numeroTotalVagoes*0.1);
+			}
+
+			double maxPeso = 0;
+			for(int i = 0; i < this.getQtdadeLocomotivas(); i++) {
+				maxPeso += this.getLocomotiva(i).getPesoMaximo();
+			}
+
+			double pesoAtual = 0;
+			for(Vagao v : vagoes) {
+				pesoAtual += v.getCapacidadeCarga();
+			}
+
+			if(this.getQtdadeVagoes() < numeroTotalVagoes && (maxPeso+vagoes.getCapacidadeCarga()) <= pesoAtual) {
+				vagoes.add(vagao);	
+				vagao.setComposicao(this);	
+			}
+		}
 		return true;
 	}
 
